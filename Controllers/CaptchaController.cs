50188ab431483514d36c083560d0f392bf2f9362
SupriyaKumari2511 <pinu2511@gmail.com>
2021-09-16 10:36:28 +0530

    First commit

---
+++ Controllers/CaptchaController.cs
@@ -0,0 +1,29 @@
+﻿using CaptchaMvc.HtmlHelpers;
+using MVCCaptcha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCaptcha.Controllers
+{
+    public class CaptchaController : Controller
+    {
+        // GET: Captcha
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(RegisterationModel objRegisterationModel)
+        {
+            if (!this.IsCaptchaValid(""))
+            {
+                ViewBag.ErrorMessage = "captcha is not valid";
+                return View("Index", objRegisterationModel);
+            }
+            return Content("captcha is valid");
+        }
+    }
+}
---
+++ Models/RegisterationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCaptcha.Models
+{
+    public class RegisterationModel
+    {   [Display(Name ="UserName")]
+        public string Username { get; set; }
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
